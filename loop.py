d22742c50b1ccbf47057ab1008a4076af1983b75
ElyorTuyliyev <tuyliyevelyor@gmail.com>
2025-12-12 16:22:16 +0500

    loop

---
+++ loop.py
@@ -0,0 +1,15 @@
+# for value in range(90):
+#     print(value, " Hello World")
+# for value in range(5, 20, 3):
+#     print(value, " Hello World")
+for value in range(10, 0, -1):
+    print(value, " Hello World")
+
+
+range("start", "end", "step")
+
+range(10, 5, -1)
+# qiymatlarni kamaytirish uchun step manfiy bolishi kerak
+
+range(1, 10, 2)
+# qiymatlarni har 2 qadamda oshirish uchun step
