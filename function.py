64c7528de484192c44117ebcc8b20d360b094f48
ElyorTuyliyev <tuyliyevelyor@gmail.com>
2025-12-12 17:33:21 +0500

    function

---
+++ function.py
@@ -0,0 +1,12 @@
+# def text(value):
+
+#     return value
+
+
+# print(text("lll"))
+
+import math
+
+gg = math.sqrt(16)
+
+print(gg)
--- loop.py
+++ loop.py
@@ -2,14 +2,32 @@
 #     print(value, " Hello World")
 # for value in range(5, 20, 3):
 #     print(value, " Hello World")
-for value in range(10, 0, -1):
-    print(value, " Hello World")
+# for value in range(10, 0, -1):
+#     if value == 5:
+#         break
+#     print(value, " Hello World")
+
+# for value in range(10, 0, -1):
+#     if value == 6:
+#         continue
+#     print(value, " Hello World")
+
+# arr = [1, 1, 1, 1]
+# arr2 = [2, 2, 2, 2]
 
 
-range("start", "end", "step")
+# for pp in arr:
+#     for e in arr2:
+#         print(pp, e)
 
-range(10, 5, -1)
-# qiymatlarni kamaytirish uchun step manfiy bolishi kerak
 
-range(1, 10, 2)
-# qiymatlarni har 2 qadamda oshirish uchun step
+for _ in range(4):
+    print("hi")
+
+# range("start", "end", "step")
+
+# range(10, 5, -1)
+# # qiymatlarni kamaytirish uchun step manfiy bolishi kerak
+
+# range(1, 10, 2)
+# # qiymatlarni har 2 qadamda oshirish uchun step
